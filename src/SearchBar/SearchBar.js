05b027f0b0c76308e76f1fe301aaac99198412f9
sunnyAli <57465011+sunnyaliiii@users.noreply.github.com>
2020-04-09 01:26:59 -0700

    Create SearchBar.js

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./SearchBar.css";
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div className="search-bar">
+        <label htmlFor="search">Search: </label>
+        <input
+          className="input-field"
+          type="text"
+          value={this.props.searchTerm}
+          id="search"
+          onChange={(e) => this.props.handleSearch(e.target.value)}
+        />
+      </div>
+    );
+  }
+}
