f3b2551a852b8c20c4e03a42c5c076fcafdc7323
sunnyAli <57465011+sunnyaliiii@users.noreply.github.com>
2020-04-09 01:23:37 -0700

    Create Book.jsx

---
+++ src/Book/Book.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Book.css";
+
+export default class Book extends Component {
+  render() {
+    return (
+      <li className="book-item">
+        <div className="left">
+          <img src={this.props.thumbnail} alt="book-thumbnail" />
+        </div>
+        <div className="right">
+          <h2>{this.props.title}</h2>
+          <p>Author: {this.props.author.join(" | ")}</p>
+          <p>Price: ${this.props.price}</p>
+          <div className="description">{this.props.description}</div>
+        </div>
+      </li>
+    );
+  }
+}
