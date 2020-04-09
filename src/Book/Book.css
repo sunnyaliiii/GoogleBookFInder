029f1cd3cd589ff24026ee496bab590cfc41bfad
sunnyAli <57465011+sunnyaliiii@users.noreply.github.com>
2020-04-09 01:23:08 -0700

    Create Book.css

---
+++ src/Book/Book.css
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Book from "../Book/Book";
+
+export default class BookList extends Component {
+  render() {
+    const books = this.props.books.map((book, index) => {
+      return <Book {...book} key={index} />;
+    });
+    return <ul className="booklist">{books}</ul>;
+  }
+}
